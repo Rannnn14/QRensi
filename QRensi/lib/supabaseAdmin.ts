dcf2805f4ec4cd2cc8c7b02c5cae448b73fb92f6
Faqih Firansyah <faqihfiransyah52@gmail.com>
2026-04-21 14:33:17 +0700

    Clean up nested QRensi folder structure

--- QRensi/lib/supabaseAdmin.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = "https://xarntfqxllizuehumayh.supabase.co"
-const serviceKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhhcm50ZnF4bGxpenVlaHVtYXloIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTgxODA5MCwiZXhwIjoyMDg3Mzk0MDkwfQ.QPld9gbiarWY5V8H19SeriO4T0d4rgnfUlLFhEI51BA"
-
-export const supabaseAdmin = createClient(supabaseUrl, serviceKey)
